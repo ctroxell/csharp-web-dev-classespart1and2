08d717a536b5723c49d904d9f60877bb6e7a3e39
Claire Troxell <cltroxell@gmail.com>
2021-11-24 13:31:19 -0600

    added Pearce's code

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -6,7 +6,12 @@ namespace SchoolPractice
     {
         public static void Main(string[] args)
         {
-            // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            // TODO: Instantiate your objects and test your exercise solutions with print statements here
+            Student claire = new Student();
+            claire.Name = "Claire";
+            claire.StudentId = 1;
+
+
         }
     }
 }
